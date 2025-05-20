de94ac86e3fa92bd236b758e752ba84aacc817ec
tresoraury <tresoraury@gmail.com>
2025-05-20 14:28:23 +0200

    code q

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,72 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col s12 m10 offset-m1 l8 offset-l2">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
             <div class="card">
-            <form  method="POST" action="{{ route('login') }}">
-                <div class="card-content">
-                    {{ csrf_field() }}
-                    <span class="card-title">{{ __('Login') }}</span>
-                    <hr>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <i class="material-icons prefix">mail</i>
-                            <input id="email" type="email" name="email" value="{{ old('email') }}" class="{{ $errors->has('email') ? 'invalid' : '' }}" required autofocus>
-                            <label for="email">{{ __('E-Mail Address') }}</label>
-                            <span class="red-text">{{ $errors->has('email') ? $errors->first('email'): '' }}</span>
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <i class="material-icons prefix">lock</i>
-                            <input id="password" type="password" name="password" class="{{ $errors->has('password') ? 'invalid' : '' }}" required>
-                            <label for="password">{{ __('Password') }}</label>
-                            <span class="red-text">{{ $errors->has('password') ? $errors->first('password'): '' }}</span>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    <p>
-                        <label>
-                            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                            <span>{{ __('Remember Me') }}</span>
-                        </label>
-                    </p>
-                    <p>
-                        <button class="btn waves-effect waves-light right" type="submit" name="action">{{ __('Login') }}
-                            <i class="material-icons right">lock_open</i>
-                        </button>
-                    </p>
-                    <br>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="card-action">
-                    @if (Route::has('password.request'))
-                        <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
-                    @endif
-                </div>
-            </form>
-        </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,67 +2,73 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'TestApp') }}</title>
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
     <!-- Styles -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @yield('css')
 </head>
 <body>
     <div id="app">
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-        @auth
-            <ul id="dropdown1" class="dropdown-content">
-                <li><a href="{{ route('logout') }}" class="logout">{{ __('Logout') }}</a></li>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            </ul>
-        @endauth
-        <nav>
-            <div class="nav-wrapper">
-                <a href="{{ url('/') }}" class="brand-logo">retour</a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                @guest
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        
-                    </ul>
-                @else
-                    <ul class="right hide-on-med-and-down">
-                        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">{{ Auth::user()->name }}<i class="material-icons right"></i></a></li>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
                     </ul>
 
-                @endguest
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </nav>
-        <ul class="sidenav" id="mobile-demo">
-            @guest
-                <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
-            @else
 
-                <li><a href="{{ route('logout') }}" class="logout">{{ __('Logout') }}</a></li>
-
-            @endguest
-        </ul>
-        @yield('content')
+        <main class="py-4">
+            @yield('content')
+        </main>
     </div>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script>
-        $(document).ready(function(){
-            $('.sidenav').sidenav();
-            $('.dropdown-trigger').dropdown();
-            $('.logout').click(function(e) {
-                e.preventDefault();
-                $('#logout-form').submit();
-            });
-        });
-    </script>
 </body>
 </html>
